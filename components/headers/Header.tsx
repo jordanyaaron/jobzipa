294506df79242e512409d48ae0cbf6be41c014d0
jorhdan <dil@DiLs-MacBook-Air.local>
2026-05-20 15:10:09 +0300

    fix

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -12,11 +12,7 @@ type HeaderProps = {
 export default function Header({ toggleDrawer }: HeaderProps) {
   const [query, setQuery] = useState("");
 
-  const handleSearch = (e) => {
-    if (e.key === "Enter" && query.trim()) {
-      navigate(`/search?q=${query}`);
-    }
-  };
+  
 
   const [mobileSearchOpen, setMobileSearchOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -38,7 +34,7 @@ export default function Header({ toggleDrawer }: HeaderProps) {
           placeholder="Search jobs..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          onKeyDown={handleSearch}
+          // onKeyDown={handleSearch}
           className="w-full text-[var(--placeholder)] pl-10 pr-4 py-2 rounded-lg border border-[var(--border)] focus:outline-none text-[var(--text)] placeholder:text-[var(--placeholder)]"
         />
       </div>
