2bf9b060d20052d9251aeb7a43eb1f21388a493b
jorhdan <dil@192.168.1.19>
2026-05-23 21:34:46 +0300

    gg

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
     "Discover your next career opportunity with JobZipa. Explore thousands of job listings, connect with top companies, and take the next step in your professional journey.",
 
   
-  themeColor: #0b1220,
+  themeColor: "#0b1220",
    manifest: "/manifest.json",
    
   viewport: {
