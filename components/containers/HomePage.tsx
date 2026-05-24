b53e77f97913c85ae56adbc838c86bd4959bcbe9
jorhdan <dil@192.168.1.19>
2026-05-24 12:03:52 +0300

    gg

--- components/containers/HomePage.tsx
+++ components/containers/HomePage.tsx
@@ -13,38 +13,41 @@ export default function  HomePage() {
   
   return (
     <>
-    <div className="w-fulll flex gap-10" >
-      {
-        loading 
-        ?   <div className="p-10 w-full lg:w-[560px] text-center text-gray-500">
-                <JobsSkeleton />
-            </div>
-        : jobs.length === 0 
-        ?   <div className="p-10 w-full lg:w-[560px] text-center text-gray-500">
-                No  jobs yet 🔖
-            </div>
-        :  <main
-              className="
-                w-full
-                pt-6
-                lg:w-[560px] 
-              "
-            >
-                <h1 className="text-xl text-[var(--text)] font-bold mb-4">Saved Jobs</h1>
-                <div className="flex flex-col gap-3">
-                    <h1>hapa ndipo save job zitakaa</h1>
+        <div className="pt-16 bg-[var(--main-bg)] px-4 md:px-6 flex-1 overflow-y-auto">
+            <main className="w-fulll flex gap-10" >
+            {
+                loading 
+                    ?   <div className="p-2 lg:p-10 w-full lg:w-[560px] text-center text-gray-500">
+                            <JobsSkeleton />
+                        </div>
+                : 
+                jobs.length === 0 
+                    ?   <div className="p-10 w-full lg:w-[560px] text-center text-gray-500">
+                            No  jobs yet 🔖
+                        </div>
+                    :  <main
+                    className="
+                        w-full
+                        pt-6
+                        lg:w-[560px] 
+                    "
+                    >
+                        <h1 className="text-xl text-[var(--text)] font-bold mb-4">Saved Jobs</h1>
+                        <div className="flex flex-col gap-3">
+                            <h1>hapa ndipo save job zitakaa</h1>
+                        </div>
+                    </main>
+            }
+            <aside className="hidden lg:block pb-10 w-[400px]">
+                <div className="sticky top-10">
+
+                    {/* ADS */}
+                    <h1>Add Section</h1>
+
                 </div>
+            </aside>
             </main>
-      }
-      <aside className="hidden lg:block pb-10 w-[400px]">
-          <div className="sticky top-10">
-
-              {/* ADS */}
-              <h1>Add Section</h1>
-
-          </div>
-      </aside>
-    </div>
+        <div/>
     </>
   );
 };
