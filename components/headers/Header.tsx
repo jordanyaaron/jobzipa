02cfe82355082f0f23f2b13e5e380c460263798c
jorhdan <dil@192.168.1.19>
2026-05-24 22:04:58 +0300

    gg

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -41,9 +41,9 @@ export default function Header({ toggleDrawer }: HeaderProps) {
 
       <Link
         href="/post"
-        className="flex items-center gap-2 px-4 py-2 rounded-lg text-white bg-text-main hover:bg-green-700"
+        className="flex items-center gap-2 px-4 py-2 rounded-lg text-background bg-foreground  hover:bg-green-700"
       >
-        <PlusIcon className="h-5 w-5 text-white" />
+        <PlusIcon className="h-5 w-5 text-background " />
         Post
       </Link>
 
