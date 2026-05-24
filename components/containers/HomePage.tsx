1828f0e6e5b4143e8c979f10ab93835960ff8d74
jorhdan <dil@192.168.1.19>
2026-05-24 12:18:20 +0300

    gg

--- components/containers/HomePage.tsx
+++ components/containers/HomePage.tsx
@@ -12,8 +12,8 @@ export default function  HomePage() {
 
   
   return (
-        <div className="absolute bottom-0 top-16 bg-[var(--main-bg)] px-4 md:px-6 flex-1 overflow-y-auto">
-            <main className="w-fulll flex gap-10" >
+        <div className="absolute  bottom-0 top-16 bg-[var(--main-bg)] px-4 md:px-6 flex-1 overflow-y-auto">
+            <main className="w-full flex gap-10" >
             {
                 loading 
                     ?   <div className="p-2 lg:p-10 w-full lg:w-[560px] text-center text-gray-500">
