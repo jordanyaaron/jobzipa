451d770737184c2f4dcc64f3ca100e827a666c23
jorhdan <dil@DiLs-MacBook-Air.local>
2026-05-27 17:15:20 +0300

    gg

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -85,7 +85,7 @@ export default function AppShell({
           className={`
             flex-1
             pt-16
-            p-4
+            p-4 min-h-screen
             ${hideNavbar ? "lg:ml-0" : "lg:ml-64"}
           `}
         >
